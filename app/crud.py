eb588e4434aecfde5b4e4a6f7255f7bb005e785b
HarshSingam <iharshsingam777@gmail.com>
2026-07-15 23:08:18 +0530

    Modification successful

--- app/crud.py
+++ app/crud.py
@@ -1,7 +1,19 @@
 from sqlalchemy.orm import Session
 from app import models ,  schemas
+from typing import Optional
 
 def create_job(db: Session , job: schemas.JobCreate):
+    existing_job = (db.query(models.JobPosting).filter(models.JobPosting.title == job.title,
+            models.JobPosting.company == job.company,
+            models.JobPosting.salary == job.salary,
+            models.JobPosting.location == job.location
+        )
+        .first())
+        
+
+    if existing_job:
+        return None
+
     db_job = models.JobPosting(
         title = job.title,
         company = job.company,
@@ -18,14 +30,16 @@ def create_job(db: Session , job: schemas.JobCreate):
 
     return db_job
 
-def get_jobs(db: Session):
-    return db.query(models.JobPosting).all()
+def get_jobs(db: Session , job_id:Optional[int]=None, company:Optional[str]=None,):
+    query = db.query(models.JobPosting)
 
-def get_job_by_id(db: Session , job_id = int ):
-    return (db.query(models.JobPosting).filter(models.JobPosting.id == job_id).first())
+    if job_id is not None:
+        query = query.filter(models.JobPosting.id == job_id)
 
-def get_job_by_company(db: Session , job_company = str):
-    return(db.query(models.JobPosting).filter(models.JobPosting.company == job_company).first())
+    if company is not None:
+        query = query.filter(models.JobPosting.company == company)
+
+    return query.all()
 
 def update_job(db: Session , job_id: int , updated_job: schemas.JobCreate):
     job = (db.query(models.JobPosting).filter(models.JobPosting.id == job_id).first())
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI ,Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from typing import Optional
 from app.connection import Base , engine
 from app.db_connection import get_db
 from app import crud, schemas
@@ -16,27 +16,29 @@ def home():
 
 @app.post("/jobs",response_model=schemas.JobResponse)
 def create_job(job: schemas.JobCreate , db: Session = Depends(get_db)):
-    return crud.create_job(db , job)
+    new_job = crud.create_job(db, job)
+
+    if new_job is None:
+        raise HTTPException(
+            status_code=409,
+            detail="This job already exists."
+        )
+
+    return new_job
 
 @app.get("/jobs", response_model=list[schemas.JobResponse])
-def get_jobs(db : Session = Depends(get_db)):
-    return crud.get_jobs(db)
+def read_jobs(
+    job_id: Optional[int] = None,
+    company: Optional[str] = None,
+    db: Session = Depends(get_db)
+):
+    jobs = crud.get_jobs(
+        db=db,
+        job_id=job_id,
+        company=company
+    )
 
-@app.get("/jobs/{job_id}", response_model=schemas.JobResponse)
-def get_job(job_id = int , db: Session = Depends(get_db)):
-    job = crud.get_job_by_id(db , job_id)
-    if job is None:
-        raise HTTPException(status_code=404, detail="Job not found")
-
-    return job
-
-@app.get("/jobs/{job_company}", response_model=schemas.JobResponse)
-def get_job(job_company = str , db: Session = Depends(get_db)):
-    job = crud.get_job_by_company(db , job_company)
-    if job is None:
-        raise HTTPException(status_code=404, detail="Job not found")
-
-    return job
+    return jobs
 
 @app.put("/jobs/{job_id}", response_model=schemas.JobResponse)
 def update_job(job_id: int, job: schemas.JobCreate, db: Session = Depends(get_db)):
