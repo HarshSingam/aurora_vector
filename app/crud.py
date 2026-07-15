06c5872f4f792746e4669b53a5569200d50e6a4a
HarshSingam <iharshsingam777@gmail.com>
2026-07-15 16:57:24 +0530

    developer branch

--- app/crud.py
+++ app/crud.py
@@ -19,3 +19,42 @@ def create_job(db: session , job: schemas.JobCreate):
 
     return db_job
 
+def get_job(db: session):
+    return db.query(models.JobPosting).all()
+
+def get_job(db: session , job_id = int ):
+    return (db.query(models.JobPosting).filter(models.JobPosting.id == job_id).first())
+
+def get_job(db: session , job_company = str):
+    return(db.query(models.JobPosting).filter(models.JobPosting.company == job_company).first())
+
+def update_job(db: session , job_id: int , updated_job: schemas.JobCreate):
+    job = (db.query(models.JobPosting).filter(models.JobPosting.id == job_id).first())
+
+    if job is None:
+        return None
+    
+    job.title = updated_job.title
+    job.company = updated_job.company
+    job.location = updated_job.location
+    job.salary = updated_job.salary
+    job.status = updated_job.status
+
+    db.commit()
+    db.refresh(job)
+
+    return job
+
+def delete_job(db: session , job_id : int):
+    job = (db.query(models.JobPosting).filter(models.JobPosting.id == job_id).first())
+
+    if job is None:
+        return None
+
+    db.delete(job)
+
+    db.commit()
+
+    return job
+
+
--- app/db_connection.py
+++ app/db_connection.py
@@ -26,3 +26,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
